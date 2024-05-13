df3ad181f04ebe03662357dcd5f10943c99b231f
namirasyalwa <164306430+namirasyalwa@users.noreply.github.com>
2024-05-13 16:24:44 +0700

    mengupdate index.js untuk membuat router ke file lain index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Todolist from '../views/Todolist.vue';
+import Posting from '../views/Posting.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Todolist',
+      component: Todolist
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/Posting',
+      name: 'Posting',
+      component: Posting
     }
   ]
 })
