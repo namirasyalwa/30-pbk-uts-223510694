778f63901e158a085f4be8271a81f5d75986a77e
namirasyalwa <164306430+namirasyalwa@users.noreply.github.com>
2024-05-13 16:25:31 +0700

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+        <nav class="navbar">
+            <router-link to="/" class="active">Todo List</router-link>
+            <router-link to="/Posting">Post</router-link>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: center; 
+    align-items: center; 
+    width: 100%; 
+}
+  
+.navbar a {
+    font-size: 1.5rem;
+    color: white;
+    margin: 0 3rem; 
+    font-weight: 700;
+    text-decoration: none; 
+}
+  
+
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 2rem 0%;
+    z-index: 1000; 
+    background: rgb(255, 204, 212);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+</style>
